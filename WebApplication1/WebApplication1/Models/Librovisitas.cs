c89f3445835d7bbcb7cced11a6b2455d2a6f1e7f
Kevin Farinango <estudiante@srvuisrael.edu.ec>
2018-11-05 19:39:18 -0500

    paso 2

---
+++ WebApplication1/WebApplication1/Models/Librovisitas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace WebApplication1.Models
+{
+	public class Librovisitas
+	{
+		public void Grabar(String nombre, String comentarios)
+		{
+			StreamWriter archivo = new StreamWriter(HostingEnvironment.MapPath("-") + "/App_Data/datos.txt", true);
+			archivo.WriteLine("Nombre" + nombre + "<br>comentarios:" + comentarios + "</br>");
+			archivo.Close();
+
+		}
+	}
+}
