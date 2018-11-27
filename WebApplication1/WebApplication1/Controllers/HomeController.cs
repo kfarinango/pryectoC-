de867bddb43aa2f7143737ecc1e8a44c0c1e8619
Master <master@srvuisrael.edu.ec>
2018-11-26 19:36:11 -0500

    semana7
    Conexión a BD MVC

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,33 +11,52 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        LibroVisitas libro = null;
+        SqlParameter[] parParameter = null;
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult FormularioVista()
+        public ActionResult FormularioVisita()
         {
             return View();
         }
-
         public ActionResult CargaDatos()
         {
-	            string nombre = Request.Form["nombre"].ToString();
-	            string comentarios = Request.Form["comentarios"].ToString();
-	            LibroVisitas libro = new LibroVisitas();
-	            libro.Grabar(nombre, comentarios);
-	            return View();
-	    }
+            string nombre = Request.Form["nombre"].ToString();
+            string comentario = Request.Form["comentarios"].ToString();
+            int opc = 2;
+            try
+            {
+                libro = new LibroVisitas();
+                parParameter = new SqlParameter[3];
 
-        public ActionResult ListadoVisitas()
-        {
-            LibroVisitas libro = new LibroVisitas();
-            string todo = libro.Leer();
-            ViewData["libro"] = todo;
+                parParameter[0] = new SqlParameter();
+                parParameter[0].ParameterName = "@opc";
+                parParameter[0].SqlDbType = SqlDbType.Int;
+                //parParameter[0].SqlValue = objContacto.opc;
+                parParameter[0].SqlValue = opc;
+
+                parParameter[1] = new SqlParameter();
+                parParameter[1].ParameterName = "@nombre";
+                parParameter[1].SqlDbType = SqlDbType.VarChar;
+                parParameter[1].Size = 50;
+                parParameter[1].SqlValue = nombre;
+
+                parParameter[2] = new SqlParameter();
+                parParameter[2].ParameterName = "@comentario";
+                parParameter[2].SqlDbType = SqlDbType.VarChar;
+                parParameter[2].Size = 500;
+                parParameter[2].SqlValue = comentario;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            libro.EjecutarSP(parParameter, "sp_comentario");
             return View();
         }
-
-
     }
 }
--- WebApplication1/WebApplication1/Models/Librovisitas.cs
+++ WebApplication1/WebApplication1/Models/Librovisitas.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -9,20 +11,49 @@ namespace WebApplication1.Models
 {
     public class LibroVisitas
     {
-        public void Grabar(string nombre, string comentarios)
+        SqlConnection cnnConexion = null;
+        SqlCommand cmdComando = null;
+        //SqlDataAdapter daAdaptador = null;
+        //DataTable dtRegresa = null;
+
+        string strCadenaConexion = string.Empty;
+        //Constructor de la clase
+        public LibroVisitas()
         {
-            StreamWriter archivo = new StreamWriter(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt", true);
-            archivo.WriteLine("Nombre:" + nombre + "<br>Comentarios:" + comentarios + "<hr>");
-            archivo.Close();
+            strCadenaConexion = @"Data Source=.;Initial Catalog=mvc_web_comentariodb;Persist Security Info=True;User ID=sa;Password=Uisrael2018";
         }
-        public string Leer()
+        public void EjecutarSP(SqlParameter[] parParameter, string nombreSP)
         {
-            StreamReader archivo = new StreamReader(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt");
-            string todo = archivo.ReadToEnd();
-            archivo.Close();
-            return todo;
-        }
+            try
+            {
+                //Instanciamos el objeto conexion con la cadena de conexion
+                cnnConexion = new SqlConnection(strCadenaConexion);
+                //Instanciamos el objeto comando con el nombreSP  y conexion a utilizar
+                cmdComando = new SqlCommand();
+                cmdComando.Connection = cnnConexion;
+                //Abrimos la conexion
+                cnnConexion.Open();
+                //Asignamos el tipo comando a ejecutar
+                cmdComando.CommandType = CommandType.StoredProcedure;
+                //Asignamos el nombre del store procedure
+                cmdComando.CommandText = nombreSP;
+                //agregamos los parametros a ejecutar
+                cmdComando.Parameters.AddRange(parParameter);
+                //Ejecutamos el TSQL enn el servidor
+                cmdComando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cnnConexion.Dispose();
+                cmdComando.Dispose();
+            }
 
 
+
+        }
     }
 }
